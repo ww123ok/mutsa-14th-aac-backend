dd3687cfc2b7c6bd2f9e9e1987804134df6f2fdb
leehanjay <128954406+leehanjay@users.noreply.github.com>
2026-08-02 00:17:26 +0900

    [feat]: cors 설정 추가

---
+++ src/main/java/mutsa/hackathon/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package mutsa.hackathon.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI swagger() {
+        Info info = new Info()
+                .title("멋쟁이사자처럼 14기 해커톤")
+                .description("일기 작성")
+                .version("0.0.1");
+
+        // JWT 토큰 헤더 방식
+        String securityScheme = "JWT TOKEN";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(securityScheme);
+        Components components = new Components()
+                .addSecuritySchemes(securityScheme, new SecurityScheme()
+                        .name(securityScheme)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
